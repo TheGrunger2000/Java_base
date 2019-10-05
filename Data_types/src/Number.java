b7aba58baf5986169145ad797d825b405f4f1686
Bogdan <grungerdiary@gmail.com>
2019-10-05 18:43:55 +0300

    05.10.19: Data types & Getters

---
+++ Data_types/src/Number.java
@@ -0,0 +1,17 @@
+class Number {
+    private byte digit_a = 13;
+    private byte digit_b = 45;
+    private int result = digit_a + digit_b;
+
+    byte getDigit_b(){
+        return digit_b;
+    }
+
+    byte getDigit_a(){
+        return digit_a;
+    }
+
+    int getResult(){
+        return result;
+    }
+}
---
+++ Data_types/src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args){
+        Number number = new Number();
+        System.out.println(number.getDigit_a());
+        System.out.println(number.getDigit_b());
+        System.out.println(number.getResult());
+    }
+}
